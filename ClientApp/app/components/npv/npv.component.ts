f8dcd74d3d2dc8978a3dc54b1c4f26d7a6e65e04
Jew Jason Yap <jew.jason.yap@towerswatson.com>
2018-06-02 19:10:23 +0800

    Initial commit

---
+++ ClientApp/app/components/npv/npv.component.ts
@@ -0,0 +1,56 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'npv',
+    templateUrl: './npv.component.html'
+})
+
+
+export class NPVComponent {
+    public parameters: NPVParameters;
+    public projects: Project[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        //http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+        //    this.parameters = result.json() as WeatherForecast[];
+        //}, error => console.error(error));
+        this.parameters = {
+            discountRateIncrement: 0.25,
+            upperBoundDiscountRate: 15,
+            lowerBoundDiscountRate: 1
+        }
+
+        this.projects = [
+            {
+                name: 'X',
+                cashFlows: [35000, 10000, 27000, 19000],
+                newCashFlow: 0
+            },
+            {
+                name: 'Y',
+                cashFlows: [35000, 27000, 27000],
+                newCashFlow: 0
+            }
+        ];
+    }
+
+    public addCashFlow(p : Project) {
+        if (p.newCashFlow > 0) {
+            p.cashFlows.push(p.newCashFlow);
+            p.newCashFlow = 0;
+        }
+    }
+}
+
+export interface Project {
+    name: string;
+    cashFlows: number[];
+    newCashFlow: number;
+}
+
+export interface NPVParameters {
+    upperBoundDiscountRate: number;
+    lowerBoundDiscountRate: number;
+    discountRateIncrement: number;
+}
