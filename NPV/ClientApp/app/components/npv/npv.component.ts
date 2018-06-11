16ad52b6be54da45300ab753e0c1583f610035fd
Jew Jason Yap <jew.jason.yap@towerswatson.com>
2018-06-11 22:28:57 +0800

    chart projection after computation

--- NPV/ClientApp/app/components/npv/npv.component.ts
+++ NPV/ClientApp/app/components/npv/npv.component.ts
@@ -20,14 +20,12 @@ export class NPVComponent {
         scaleShowVerticalLines: false,
         responsive: true
     };
-    public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    public barChartType: string = 'bar';
+    
+    public barChartLabels: string[] = [];
+    public barChartType: string = 'line';
     public barChartLegend: boolean = true;
 
-    public barChartData: any[] = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-    ];
+    public barChartData: any[] = [];
 
     // events
     public chartClicked(e: any): void {
@@ -95,7 +93,23 @@ export class NPVComponent {
             , options)
             .subscribe(result => {
                 let npvRates = result.json();
+
+                //set tabular npv results per rate
                 p.ratesnpv = npvRates;
+
+                //set visualization labels and data
+                this.barChartLabels = p.ratesnpv.map((x) => {
+                    return x.rate.toString();
+                });
+
+                let chartData = p.ratesnpv.map((x) => {
+                    return x.npv.toString();
+                });
+
+                this.barChartData.push({
+                    data: chartData,
+                    label: 'Project ' + p.name
+                });
 
             }, error => console.error(error));
     }
